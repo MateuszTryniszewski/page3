1181662a63c786f9af9876b1da96c7dda5cfebad
Mateusz Tryniszewski <trynio132@gmail.com>
2018-10-31 13:37:56 +0100

    add scrollreveal

--- js/main.js
+++ js/main.js
@@ -1,4 +1,23 @@
 document.getElementById('hamburger').addEventListener("click",function(){
      document.getElementById('mobile').classList.toggle('show');
+});
 
-})
+window.sr = ScrollReveal();
+sr.reveal('.flex-item', {
+    reset: false,
+    delay: 300,
+    distance: '100px',
+    origin: 'right'
+});
+sr.reveal('img,i,.about-content', {
+    reset: false,
+    delay: 500,
+    distance: '100px',
+    origin: 'right'
+});
+sr.reveal('p,h1,h2,h3,.col', {
+    reset: false,
+    delay: 500,
+    distance: '100px',
+    origin: 'right'
+});
